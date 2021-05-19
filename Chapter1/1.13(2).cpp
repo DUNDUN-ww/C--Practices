acf6bc7bbda5bde7ac5c78664f9cf60a1dc9645b
DunDun <74822350+DUNDUN-ww@users.noreply.github.com>
2021-05-19 20:03:51 +0800

    Add files via upload

---
+++ Chapter1/1.13(2).cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+int main()
+{
+    int smallest = 0, biggest = 0;
+    std::cout << "please input two integers:";
+    std::cin >> smallest >> biggest;
+
+    if (smallest > biggest) {//首先按照判断输入数字大小，反了就调换
+        int exchange = smallest;
+        smallest = biggest;
+        biggest = exchange;
+    }
+
+    for (int i = smallest; i <= biggest; i++) {
+        std::cout << i << " ";
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
