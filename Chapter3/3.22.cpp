75115bf727002a40a24ab73e4da341a9fb6cd776
DunDun <74822350+DUNDUN-ww@users.noreply.github.com>
2021-05-23 23:53:53 +0800

    Update 3.22.cpp

--- Chapter3/3.22.cpp
+++ Chapter3/3.22.cpp
@@ -1,1 +1,27 @@
-哦
+#include <iostream>
+#include <vector>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	vector<string> text;
+	text.push_back("a b c");
+
+
+	for (auto it = text.begin(); it != text.end() && !it->empty(); ++it)
+	{
+		for (auto &c : *it)
+		{
+			if (isalpha(c)) c = toupper(c);
+		}
+	}
+
+	for (auto it : text)
+	{
+		cout << it << endl;
+	}
+	return 0;
+}
