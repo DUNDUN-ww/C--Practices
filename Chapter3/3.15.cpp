f233d6d2b94cd1aa067ad0ae093b32bba2e172ff
DunDun <74822350+DUNDUN-ww@users.noreply.github.com>
2021-05-23 20:34:19 +0800

    Create 3.15.cpp

---
+++ Chapter3/3.15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	string str;
+	vector<string> strVec;
+	while (cin >> str)
+	{
+		strVec.push_back(str);
+	}
+
+	return 0;
+}
