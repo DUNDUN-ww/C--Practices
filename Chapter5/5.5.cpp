7cb5f64056243db32081af7d3746f337afe5b177
DunDun <74822350+DUNDUN-ww@users.noreply.github.com>
2021-05-25 13:44:56 +0800

    Create 5.5.cpp

---
+++ Chapter5/5.5.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int grade;
+	cout << "please enter your grade" << endl;
+	cin >> grade;
+	if (grade < 0 || grade>100)
+	{
+		cout << "the grade is illegal" << endl;
+		return -1;
+	}
+	else if (grade == 100)  //处理满分
+	{
+		cout << "the grade is A++" << endl;
+		return -1;
+	}
+	else if (grade < 60)    //处理不及格
+	{
+		cout << "the grade is F" << endl;
+		return -1;
+	}
+	int iu = grade / 10;    //成绩的十位数
+	int it = grade % 10;    //成绩的个位数
+	string score, level, lettergrade;
+	//根据成绩的十位数字确定score
+	if (iu == 9)
+	{
+		score = "A";
+	}
+	else if (iu == 8)
+	{
+		score = "B";
+	}
+	else if (iu == 7)
+	{
+		score = "c";
+	}
+	else
+		score = "D";
+	//根据成绩个位数字确定level
+	if (it < 3)
+	{
+		level = "-";
+	}
+	else if (it > 7)
+	{
+		level = "+";
+	}
+	else
+	{
+		level = "";
+	}
+	//累加求得等级成绩
+	lettergrade = score + level;
+	cout << "the grade is:" << lettergrade << endl;
+	return 0;
+}
