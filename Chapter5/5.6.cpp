436b0801ef89ba0e68931f3ae21e00ebea95e8b0
DunDun <74822350+DUNDUN-ww@users.noreply.github.com>
2021-05-25 13:53:26 +0800

    Update and rename 5.7.cpp to 5.6.cpp

--- Chapter5/5.6.cpp
+++ Chapter5/5.6.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int main()
 {
 	int grade;
-	cout << "please enter your grade" << endl;
+	cout << "请输入您的成绩" << endl;
 	cin >> grade;
 	if (grade < 0 || grade>100)
 	{
-		cout << "the grade is illegal" << endl;
+		cout << "成绩输入错误" << endl;
 		return -1;
 	}
 	else if (grade == 100)  //处理满分
 	{
-		cout << "the grade is A++" << endl;
+		cout << "您的成绩等级是 A++" << endl;
 		return -1;
 	}
 	else if (grade < 60)    //处理不及格
 	{
-		cout << "the grade is F" << endl;
+		cout << "您的成绩等级是 F" << endl;
 		return -1;
 	}
 	int iu = grade / 10;    //成绩的十位数
@@ -33,6 +33,6 @@ int main()
 		: (it > 7) ? "+" : "";
 	//累加求得等级成绩
 	lettergrade = score + level;
-	cout << "the grade is:" << lettergrade << endl;
+	cout << "您的成绩等级是：" << lettergrade << endl;
 	return 0;
 }
