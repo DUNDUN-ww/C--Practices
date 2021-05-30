510c798ee43384dd734f97ff36cff8debed249fc
DunDun <74822350+DUNDUN-ww@users.noreply.github.com>
2021-05-30 20:42:29 +0800

    Create 6.10.cpp

---
+++ Chapter6/6.10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int exchange(int& val1, int& val2)//函数的声明及定义，接受两个参数，需要以传址的方式传入(引用)
+{
+	int a;
+	a = val1;
+	val1 = val2;
+	val2 = a;
+	return 0;
+}
+void main()
+{
+	cout << "请输入需要交换的两整数:";
+	int val1, val2;
+	cin >> val1 >> val2;
+	cout << "交换之前的两数：" << val1 << " " << val2 << endl;
+	exchange(val1, val2);
+	cout << "交换之后的两数：" << val1 << " " << val2 << endl;
+}
