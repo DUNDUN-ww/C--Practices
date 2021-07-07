e367ac07125b4afccdc14cbf1e547b11964efc14
DunDun <74822350+DUNDUN-ww@users.noreply.github.com>
2021-07-07 10:08:29 +0800

    Create 7.23.cpp

---
+++ Chapter7/7.23.cpp
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+class Screen {
+public:
+    using pos = string::size_type;
+
+    Screen() = default;
+    Screen(pos ht, pos wd, char c) :height(ht), width(wd), contents(ht* wd, c) { }
+
+    char get() const { return contents[cursor]; }
+    char get(pos r, pos c) const { return contents[r * width + c]; }
+
+private:
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    string contents;
+};
