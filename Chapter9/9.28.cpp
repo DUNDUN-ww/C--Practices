f3c47839fdcd1fb127fae453f0b7e291e1c03009
DunDun <74822350+DUNDUN-ww@users.noreply.github.com>
2021-07-22 23:31:24 +0800

    Create 9.28.cpp

---
+++ Chapter9/9.28.cpp
@@ -0,0 +1,16 @@
+void find_and_insert(forward_list<string>& flst, const string& s1, const string& s2)
+{
+	auto prev = flst.before_begin();
+	auto curr = flst.begin();
+	while (curr != flst.end())
+	{
+      if (*curr == s1)
+      {
+        flst.insert_after(curr, s2);
+        return;
+        }
+	    prev = curr;
+	    ++curr;
+    }
+    flst.insert_after(prev, s2);
+}
